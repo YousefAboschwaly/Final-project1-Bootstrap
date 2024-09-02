86a0976bb8143821e38f7b6eaea40a9109bbfd9e
yousef aboschwaly <116332558+YousefAboschwaly@users.noreply.github.com>
2024-09-02 18:23:02 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,8 @@
+  "use strict";
+const typed = new Typed(".moveLetter", {
+  strings: ["Larry Daniels", "Developer", "Designer"],
+  loop: true,
+  typeSpeed: 35,
+  backSpeed: 35,
+  backDelay: 1500,
+});
